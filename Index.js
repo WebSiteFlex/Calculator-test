2b115ab86274611ccca5cff3c2cc9beb0f464f52
uREwebsites <julianperez620@gmail.com>
2023-04-16 16:01:54 -0300

    modificaciones

---
+++ Index.js
@@ -0,0 +1,40 @@
+function sumar (){
+    let num = [];
+    let num2 = [];
+
+    resultado = num[0] + num2 [0];
+
+    console.log(resultado);
+
+};
+
+function restar (){
+ let num = [];
+ let num2 =[];
+ 
+  resultado = num[0] - num2 [0];
+ 
+  console.log(resultado);
+};
+
+function dividir() {
+  let num = [];
+  let num2 = []; 
+
+  resultado = num[0] / num2[0];
+  
+  console.log(resultado);
+};
+
+function multiplciar () {
+let num = [];
+let num2 = [];
+
+resultado = num[0] * num2[0];
+
+console.log(resultado);
+
+}; 
+
+
+
